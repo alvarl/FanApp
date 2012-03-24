854fd2f9e86078886f15c4d2c46a4617c5a6f6f4
Alvar Lumberg <alvar@codeborne.com>
2012-03-24 15:44:30 +0200

    added Production entity

---
+++ app/models/Production.java
@@ -0,0 +1,22 @@
+package models;
+
+import play.db.jpa.*;
+
+import javax.persistence.*;
+import java.util.*;
+
+@Entity(name = "web_event")
+public class Production extends GenericModel {
+  @Id @Column(name = "page_id")
+  public Long id;
+  @Column(name = "d_title_est")
+  public String title;
+  @Column(name = "d_intro_est")
+  public String intro;
+  @OneToMany()
+  public List<Event> events;
+
+  public Production() {
+  }
+
+}
