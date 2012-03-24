46ff8758247ad4def62c9f411f5102b7f1f3b70c
Ando Roots <ando@roots.ee>
2012-03-24 11:48:53 +0200

    Identity provider SQL

--- doc/db/create_schema.sql
+++ doc/db/create_schema.sql
@@ -6,16 +6,35 @@ CREATE SCHEMA IF NOT EXISTS `d17556sd47469` ;
 USE `d17556sd47469` ;
 
 -- -----------------------------------------------------
--- Table `d17556sd47469`.`fan_users`
+-- Table `d17556sd47469`.`fan_user`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `d17556sd47469`.`fan_users` ;
+DROP TABLE IF EXISTS `d17556sd47469`.`fan_user` ;
 
-CREATE  TABLE IF NOT EXISTS `d17556sd47469`.`fan_users` (
+CREATE  TABLE IF NOT EXISTS `d17556sd47469`.`fan_user` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `email` VARCHAR(32) NOT NULL ,
   `facebook_id` INT NULL DEFAULT NULL ,
   `phone` VARCHAR(32) NULL DEFAULT NULL ,
   PRIMARY KEY (`id`) )
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `d17556sd47469`.`fan_user_identity`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `d17556sd47469`.`fan_user_identity` ;
+
+CREATE  TABLE IF NOT EXISTS `d17556sd47469`.`fan_user_identity` (
+  `user_id` INT NOT NULL ,
+  `provider_id` VARCHAR(64) NOT NULL ,
+  `type` VARCHAR(32) NULL ,
+  INDEX `fk_user_identity_fan_user` (`user_id` ASC) ,
+  PRIMARY KEY (`user_id`, `provider_id`) ,
+  CONSTRAINT `fk_user_identity_fan_user`
+    FOREIGN KEY (`user_id` )
+    REFERENCES `d17556sd47469`.`fan_user` (`id` )
+    ON DELETE SET NULL
+    ON UPDATE SET NULL)
 ENGINE = InnoDB;
 
 
