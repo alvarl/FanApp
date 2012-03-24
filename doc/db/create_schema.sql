15c8a600748b3b2fceb48c81cf7d80f57a223d86
Ando Roots <ando@roots.ee>
2012-03-24 11:57:06 +0200

    Fix providerid table columns

--- doc/db/create_schema.sql
+++ doc/db/create_schema.sql
@@ -25,6 +25,7 @@ DROP TABLE IF EXISTS `fan_user_identity` ;
 CREATE  TABLE IF NOT EXISTS `fan_user_identity` (
   `user_id` INT NOT NULL ,
   `type` VARCHAR(32) NOT NULL ,
+  `provider_id` VARCHAR(128) NOT NULL ,
   INDEX `fk_user_identity_fan_user` (`user_id` ASC) ,
   PRIMARY KEY (`user_id`, `type`) ,
   CONSTRAINT `fk_user_identity_fan_user`
