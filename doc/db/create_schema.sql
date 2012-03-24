c46b15bc7b6fae1856cde00a06ddefbc50f1b9dd
Ando Roots <ando@roots.ee>
2012-03-24 11:52:53 +0200

    Fix providerid table columns

--- doc/db/create_schema.sql
+++ doc/db/create_schema.sql
@@ -2,15 +2,13 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
-CREATE SCHEMA IF NOT EXISTS `d17556sd47469` ;
-USE `d17556sd47469` ;
 
 -- -----------------------------------------------------
--- Table `d17556sd47469`.`fan_user`
+-- Table `fan_user`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `d17556sd47469`.`fan_user` ;
+DROP TABLE IF EXISTS `fan_user` ;
 
-CREATE  TABLE IF NOT EXISTS `d17556sd47469`.`fan_user` (
+CREATE  TABLE IF NOT EXISTS `fan_user` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `email` VARCHAR(32) NOT NULL ,
   `facebook_id` INT NULL DEFAULT NULL ,
@@ -20,19 +18,18 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `d17556sd47469`.`fan_user_identity`
+-- Table `fan_user_identity`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `d17556sd47469`.`fan_user_identity` ;
+DROP TABLE IF EXISTS `fan_user_identity` ;
 
-CREATE  TABLE IF NOT EXISTS `d17556sd47469`.`fan_user_identity` (
+CREATE  TABLE IF NOT EXISTS `fan_user_identity` (
   `user_id` INT NOT NULL ,
-  `provider_id` VARCHAR(64) NOT NULL ,
-  `type` VARCHAR(32) NULL ,
+  `type` VARCHAR(32) NOT NULL ,
   INDEX `fk_user_identity_fan_user` (`user_id` ASC) ,
-  PRIMARY KEY (`user_id`, `provider_id`) ,
+  PRIMARY KEY (`user_id`, `type`) ,
   CONSTRAINT `fk_user_identity_fan_user`
     FOREIGN KEY (`user_id` )
-    REFERENCES `d17556sd47469`.`fan_user` (`id` )
+    REFERENCES `fan_user` (`id` )
     ON DELETE SET NULL
     ON UPDATE SET NULL)
 ENGINE = InnoDB;
