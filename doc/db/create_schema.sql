38feb2db8980e0fd856c1287af55a1c06dd11a85
Ando Roots <ando@roots.ee>
2012-03-24 16:32:19 +0200

    Add fan_event_rating table

--- doc/db/create_schema.sql
+++ doc/db/create_schema.sql
@@ -11,10 +11,10 @@ DROP TABLE IF EXISTS `fan_user` ;
 CREATE  TABLE IF NOT EXISTS `fan_user` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `email` VARCHAR(32) NOT NULL ,
-  `facebook_id` INT NULL DEFAULT NULL ,
   `phone` VARCHAR(32) NULL DEFAULT NULL ,
   `name` VARCHAR(255) NULL DEFAULT 'Teatrihuviline' ,
-  PRIMARY KEY (`id`) )
+  PRIMARY KEY (`id`) ,
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) )
 ENGINE = InnoDB;
 
 
@@ -24,17 +24,37 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `fan_user_identity` ;
 
 CREATE  TABLE IF NOT EXISTS `fan_user_identity` (
-  `user_id` INT NOT NULL ,
   `type` VARCHAR(32) NOT NULL ,
   `provider_id` VARCHAR(128) NOT NULL ,
-  INDEX `fk_user_identity_fan_user` (`user_id` ASC) ,
-  PRIMARY KEY (`user_id`, `type`) ,
-  CONSTRAINT `fk_user_identity_fan_user`
+  `user_id` INT NOT NULL ,
+  PRIMARY KEY (`type`) ,
+  INDEX `fk_fan_user_identity_fan_user` (`user_id` ASC) ,
+  CONSTRAINT `fk_fan_user_identity_fan_user`
     FOREIGN KEY (`user_id` )
     REFERENCES `fan_user` (`id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `fan_event_rating`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `fan_event_rating` ;
+
+CREATE  TABLE IF NOT EXISTS `fan_event_rating` (
+  `fan_user_id` INT NOT NULL ,
+  `event_id` INT(11) NOT NULL COMMENT 'Link to MYISAM web_event' ,
+  `rating` VARCHAR(32) NOT NULL ,
+  INDEX `fk_fan_event_rating_fan_user1` (`fan_user_id` ASC) ,
+  PRIMARY KEY (`event_id`, `fan_user_id`) ,
+  CONSTRAINT `fk_fan_event_rating_fan_user1`
+    FOREIGN KEY (`fan_user_id` )
+    REFERENCES `fan_user` (`id` )
+    ON DELETE SET NULL
+    ON UPDATE SET NULL)
+ENGINE = InnoDB
+COMMENT = 'Stores like/dislike';
 
 
 
