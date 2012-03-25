24f73ce1d67d59e3e99a5fe86aedfdccb512ad8a
Alvar Lumberg <alvar@codeborne.com>
2012-03-25 11:52:00 +0300

    Handle actor patting and display status message

---
+++ app/models/Reward.java
@@ -0,0 +1,13 @@
+package models;
+
+public class Reward {
+
+  public String name;
+  public int points;
+  public int id;
+
+  public Reward(String name, int points) {
+    this.name = name;
+    this.points = points;
+  }
+}
