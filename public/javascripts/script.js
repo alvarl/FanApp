4262b9926154e5749461c63c83a4d484ab3ac181
unknown <robert@furamo.(none)>
2012-03-25 11:09:37 +0300

    point scrolling

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,41 +1,9 @@
-$(document).ready(function() {
-
-});
-var sliding = false;
-function toggleKavaExtended(item, id)
+function slideNum99()
 {
-    if(sliding)
-        return;
-    sliding = true;
-    var ext = $(item).parent().find('.extended');
-    var exti = ext.get(0);
-    loaded = exti.loaded;
-    if(!loaded)
-    {
-        ext.load('/Application/kava_ext', function(){
-            $(this).slideDown(500, function(){
-                sliding = false;
-                $(this).get(0).loaded = true;
-                $(this).get(0).shown = true;
-            });
-        });
-    }
-    else
-    {
-        shown = exti.shown;
-        if(shown)
-        {
-            ext.slideUp(500, function(){
-                sliding = false;
-            });
-            exti.shown = false;
-        }
-        else
-        {
-            ext.slideDown(500, function(){
-                sliding = false;
-            });
-            exti.shown = true;
-        }
-    }
+    $('.num99').animate({
+        'top': '-100px'
+    }, 1000);
+    $('.num-user').animate({
+        'top': '0'
+    }, 1000);
 }
